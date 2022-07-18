5ecb178f14f14fae7e19d8ef85e27e58bba093e2
Diana Huang <dianahuang@Dianas-MBP.lan>
2022-07-18 11:43:30 -1000

    forum-app

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, render_template, redirect, session, flash
+from regex import E
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Feedback
+from forms import LoginForm, RegisterForm, FeedbackForm
+from sqlalchemy.exc import IntegrityError
+import os
+import re
+
+
+app = Flask(__name__)
+
+
+uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+# , "postgresql:///flask_feedback"
+
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "iamsecret")
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def home_page():
+    if "username" not in session:
+        return redirect("/register")
+    posts = Feedback.query.all()
+    return render_template("show_posts.html", posts=posts)
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        new_user = User.register(username, password, email, first_name, last_name)
+
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append("Username taken.  Please pick another")
+            return render_template("register.html", form=form)
+        session["username"] = new_user.username
+        flash("Welcome! Successfully Created Your Account!", "success")
+        return redirect(f"/users/{new_user.username}")
+
+    return render_template("register.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}!", "primary")
+            session["username"] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ["Invalid username/password."]
+
+    return render_template("login.html", form=form)
+
+
+@app.route("/users/<username>")
+def show_user_info(username):
+    if "username" not in session:
+        flash("Please log in first!", "danger")
+        return redirect("/")
+    if not User.query.filter_by(username=username).first():
+        flash("User does not exist", "danger")
+        return redirect(f"/users/{session['username']}")
+
+    user = User.query.filter_by(username=username).first()
+
+    return render_template("user_info.html", user=user)
+
+
+@app.route("/logout")
+def logout():
+    if "username" not in session:
+        flash("You are already logged out!")
+        return redirect("/")
+    session.pop("username")
+    flash("Goodbye!", "info")
+    return redirect("/")
+
+
+@app.route("/users/<username>/feedback/add", methods=["GET", "POST"])
+def show_feedback_form(username):
+    if "username" not in session:
+        flash("Please log in first!", "danger")
+        return redirect("/")
+    user = User.query.filter_by(username=username).first()
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        new_feedback = Feedback(title=title, content=content, username=username)
+        db.session.add(new_feedback)
+        db.session.commit()
+        flash("Your post was created", "success")
+        return redirect(f"/users/{username}")
+
+    return render_template("new_feedback.html", user=user, form=form)
+
+
+@app.route("/users/<username>/delete", methods=["POST"])
+def delete_user(username):
+    session.pop("username")
+    user = User.query.filter_by(username=username).first()
+    db.session.delete(user)
+    db.session.commit()
+    flash("User deleted", "danger")
+    return redirect("/")
+
+
+@app.route("/feedback/<feedback_id>/update", methods=["GET", "POST"])
+def edit_feedback_form(feedback_id):
+    if "username" not in session:
+        flash("Please log in first!", "danger")
+        return redirect("/")
+
+    if not Feedback.query.get(feedback_id):
+        flash("Post does not exist", "danger")
+        return redirect(f"/users/{session['username']}")
+
+    feedback = Feedback.query.get(feedback_id)
+    form = FeedbackForm(obj=feedback)
+
+    if form.validate_on_submit():
+        feedback.title = form.title.data
+        feedback.content = form.content.data
+        db.session.commit()
+        flash("Your post was edited", "success")
+        return redirect(f"/users/{feedback.username}")
+
+    return render_template("edit_feedback.html", feedback=feedback, form=form)
+
+
+@app.route("/feedback/<feedback_id>/delete", methods=["POST"])
+def delete_feedback(feedback_id):
+    if "username" not in session:
+        flash("Please log in first!", "danger")
+        return redirect("/")
+
+    if not Feedback.query.get(feedback_id):
+        flash("Post does not exist", "danger")
+        return redirect(f"/users/{session['username']}")
+
+    feedback = Feedback.query.get(feedback_id)
+    db.session.delete(feedback)
+    db.session.commit()
+    return redirect(f"/users/{feedback.username}")
---
+++ models.py
@@ -0,0 +1,81 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    username = db.Column(db.String(20), nullable=False, unique=True)
+
+    password = db.Column(db.Text, nullable=False)
+
+    email = db.Column(db.String(50), unique=True)
+
+    first_name = db.Column(db.String(30), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @validates("email")
+    def validate_email(self, key, address):
+        if "@" not in address:
+            raise ValueError("failed simple email validation")
+        return address
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user w/username and hashed pwd
+        return cls(
+            username=username,
+            password=hashed_utf8,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # return user instance
+            return u
+        else:
+            return False
+
+    feedback = db.relationship("Feedback", backref="user", cascade="all, delete-orphan")
+
+
+class Feedback(db.Model):
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey("users.username"))
